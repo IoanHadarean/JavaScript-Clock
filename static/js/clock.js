4d472fd838405b30b7470a78994e20fbdd4d1edf
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-24 15:34:31 +0000

    Fixed glitch when seconds reach 90 degrees

--- static/js/clock.js
+++ static/js/clock.js
@@ -1,19 +1,29 @@
 const secondsHand = document.querySelector('.seconds-hand');
 const minutesHand = document.querySelector('.minutes-hand');
 const hoursHand = document.querySelector('.hour-hand');
+const hands = document.querySelectorAll('.clock__hand');
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = (seconds / 60) * 360 + 90;
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
     const minutes = now.getMinutes();
-    const minutesDegrees = (minutes / 60) * 360 + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
     minutesHand.style.transform = `rotate(${minutesDegrees}deg`;
     const hours = now.getHours();
-    const hoursDegrees = (hours / 12) * 360 + 90;
+    const hoursDegrees = ((hours / 12) * 360) + 90;
     hoursHand.style.transform = `rotate(${hoursDegrees}deg`;
-    
+
+    if (secondsDegrees === 90) {
+        hands.forEach(hand => hand.style.transition = 'none');
+    }
+    else {
+        hands.forEach(hand => hand.style.transition = '');
+    }
 }
+
+
+
 
 setInterval(setDate, 1000);
